4cf6c778c4b7152233cd18b117d37eaeddf6160b
art-henry <65597700+art-henry@users.noreply.github.com>
2023-08-02 10:53:50 +0200

    Update script.js

--- pulse/js/script.js
+++ pulse/js/script.js
@@ -1,22 +1,4 @@
-// $(function () {
-//   $(".carousel__inner").slick({
-//     speed: 1200,
-// adaptiveHeight: true,
-//     prevArrow:
-//       '<button type="button" class="slick-prev"><img src="icon/icon_left.png"/></button>',
-//     nextArrow:
-//       '<button type="button" class="slick-next"><img src="icon/icon_right.png"/></button>',
-//     responsive: [
-//       {
-//         breakpoint: 992,
-//         settings: {
-//           dots: true,
-//           arrows: false,
-//         },
-//       },
-//     ],
-//   });
-// });
+
 
 document.addEventListener("DOMContentLoaded", function () {
   // Slider
@@ -178,48 +160,3 @@ document.addEventListener("DOMContentLoaded", function () {
 
   new WOW().init();
 
-  // const buttons = document.querySelectorAll('[data-modal="consultation"]');
-  // const orderButtons = document.querySelectorAll('[data-modal="order"]');
-  // const modal = document.querySelector("#consultation");
-  // const orderModal = document.querySelector("#order");
-  // const overlay = document.querySelector(".overlay");
-
-  // // Функция для открытия модального окна
-  // function openModal() {
-  //   modal.style.display = "block";
-  //   overlay.style.display = "block";
-  // }
-
-  // function openOrderModal() {
-  //   orderModal.style.display = "block";
-  //   overlay.style.display = "block";
-  // }
-
-  // // Функция для закрытия модального окна
-  // function closeModal() {
-  //   modal.style.display = "none";
-  //   overlay.style.display = "none";
-  //   orderModal.style.display = "none";
-  // }
-
-  // // Назначаем обработчик события клика на каждую кнопку
-  // buttons.forEach(function (button) {
-  //   button.addEventListener("click", openModal);
-  // });
-  // orderButtons.forEach(function (button) {
-  //   button.addEventListener("click", openOrderModal);
-  // });
-
-  // // Назначаем обработчик события клика на оверлей для закрытия модального окна
-  // overlay.addEventListener("click", function (event) {
-  //   if (event.target === overlay) {
-  //     closeModal();
-  //   }
-  // });
-
-  // // Назначаем обработчик события клика на кнопку закрытия модального окна
-  // const closeButton = modal.querySelector(".modal__close");
-  // const orderCloseButton = orderModal.querySelector(".modal__close");
-  // closeButton.addEventListener("click", closeModal);
-  // orderCloseButton.addEventListener("click", closeModal);
-});
